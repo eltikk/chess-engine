f9375c6d3a5de33331296c2986fb994a20008c1f
eltikk <eltikallaba@gmail.com>
2026-07-24 21:52:56 +0200

    feat(movegen): implement attack tables for knights, kings and pawns

--- include/MoveGenerator.h
+++ include/MoveGenerator.h
@@ -4,10 +4,19 @@
 
 class MoveGenerator{
 public:
-    static void initializeKnightAttacks();
-    static std::uint64_t getKnightAttacks(int square){
-        return knightAttacks[square];
-    }
+    static void init();
+
+    static std::uint64_t getKnightAttacks(int square){return knightAttacks[square];}
+    static std::uint64_t getKingAttacks(int square){return kingAttacks[square];}
+    static std::uint64_t getPawnAttacks(int square, bool isWhite){
+        return pawnAttacks[isWhite? 0 : 1][square];}
+
 private:
+    static void initializeKnightAttacks();
+    static void initializeKingAttacks();
+    static void initializePawnAttacks();
+
     inline static std::uint64_t knightAttacks[64];
+    inline static std::uint64_t kingAttacks[64];
+    inline static std::uint64_t pawnAttacks[2][64];
 };
--- src/MoveGenerator.cpp
+++ src/MoveGenerator.cpp
@@ -1,25 +1,47 @@
 #include "MoveGenerator.h"
 
-void MoveGenerator::initializeKnightAttacks(){
-    int file;
-    int rank;
-    const int fileChecker[8]={1,1,-1,-1,2,2,-2,-2};
-    const int rankChecker[8]={2,-2,2,-2,1,-1,1,-1};
-    for(int i = 0; i < 64; i++){
-        file = i % 8;
-        rank = i / 8;
-        std::uint64_t attack = 0;
 
-        for(int j = 0; j < 8; j++){
-            int newFile = file + fileChecker[j];
-            int newRank = rank + rankChecker[j];
-            if(newFile >=0 && newFile < 8 && newRank >=0 && newRank < 8){
-                int destination = newRank * 8 + newFile;
-                attack |= 1ULL << destination;
-            }
-        }
-        knightAttacks[i] = attack;   
-    }
+constexpr std::uint64_t NOT_A  = 0xFEFEFEFEFEFEFEFEULL; // Excludes File A
+constexpr std::uint64_t NOT_H  = 0x7F7F7F7F7F7F7F7FULL; // Excludes File H
+constexpr std::uint64_t NOT_AB = 0xFCFCFCFCFCFCFCFCULL; // Excludes Files A & B
+constexpr std::uint64_t NOT_GH = 0x3F3F3F3F3F3F3F3FULL; // Excludes Files G & H
+
+void MoveGenerator::init(){
+    initializeKnightAttacks();
+    initializeKingAttacks();
+    initializePawnAttacks();
 }
 
 
+void MoveGenerator::initializeKnightAttacks(){
+    for(int i = 0; i < 64;i++){
+        std::uint64_t b = 1ULL << i;
+        
+        knightAttacks[i] = (((b << 17) | (b >> 15)) & NOT_A)
+                         | (((b << 10) | (b >> 6)) & NOT_AB)
+                         | (((b << 15) | (b >> 17)) & NOT_H)
+                         | (((b << 6)  | (b >> 10)) & NOT_GH);     
+    }
+}
+
+void MoveGenerator::initializeKingAttacks(){
+    for(int i = 0; i < 64; i++){
+        std::uint64_t b = 1ULL << i;
+
+        kingAttacks[i] = ((b << 8) | (b >> 8))
+                       | (((b << 1) | (b << 9) | (b >> 7)) & NOT_A)
+                       | (((b >> 1) | (b >> 9) | (b << 7)) & NOT_H);
+    }
+}
+
+void MoveGenerator::initializePawnAttacks(){
+    for(int i =0 ; i < 64; i++){
+        std::uint64_t b = 1ULL << i;
+
+        //White Pawn Attacks
+        pawnAttacks[0][i] = ((b << 7) & NOT_H) | ((b<<9) & NOT_A);
+
+        //Black Pawn Attacks
+        pawnAttacks[1][i] = ((b >> 7) & NOT_A) | ((b>>9) & NOT_H);
+     }
+}
